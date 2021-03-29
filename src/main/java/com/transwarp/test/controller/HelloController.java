f6d7dff1e269130badc5573510f19ee0a318a63b
oldwong77 <2681086480@qq.com>
2021-03-29 16:58:34 +0800

    commit

--- src/main/java/com/transwarp/test/controller/HelloController.java
+++ src/main/java/com/transwarp/test/controller/HelloController.java
@@ -4,7 +4,14 @@ import com.transwarp.test.config.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class HelloController {
@@ -14,12 +21,29 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String hello(Model model) {
-
         // 注入配置文件到resource
-        model.addAttribute("resource",resource);
-
+        model.addAttribute("resource", resource);
         model.addAttribute("name", "wong");
         return "freemarker/hello";
+    }
+
+    @GetMapping("/user")
+    public String center(ModelMap map) {
+        map.put("users", parseUsers());
+        map.put("title", "用户列表");
+        return "freemarker/center/center";
+    }
+
+    private List<Map> parseUsers() {
+        List<Map> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Map map = new HashMap();
+            map.put("name", "kevin_" + i);
+            map.put("age", 10 + i);
+            map.put("phone", "1860291105" + i);
+            list.add(map);
+        }
+        return list;
     }
 
 }
--- src/main/resources/templates/freemarker/hello.ftl
+++ src/main/resources/templates/freemarker/hello.ftl
@@ -10,5 +10,22 @@ Hello ${name}
 <h1>${resource.name}</h1>
 <h1>${resource.website}</h1>
 <h1>${resource.language}</h1>
+
+<#--user交给后端响应，localhost/user响应-->
+<a href="user">green mouse</a>
+
+<br>
+<#--支持动态的href-->
+<a href="${resource.name}">${resource.name}</a>
+
+<#--if用法-->
+<h1>
+    Welcome ${resource.name}
+    <#if resource.name == "user">, our beloved leader</#if>!
+</h1>
+
+<#--包含页面-->
+<#--<#include "/copyright_footer.html">-->
+
 </body>
 </html>
